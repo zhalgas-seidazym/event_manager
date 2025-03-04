8f9b63e909b331b404cae87cbdb5fdafd0849ad0
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-03-04 23:32:46 +0500

    added login, logout functions

--- eventify/views.py
+++ eventify/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def loginController(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == "POST":
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            User.objects.get(username = username)
+        except:
+            messages.error(request, 'User does not exist')
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    context = {'page': page}
+    return render(request, 'eventify/login_register.html', context)
+
+@login_required(login_url='login')
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
