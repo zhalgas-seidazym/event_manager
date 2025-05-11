23d200d352d413fbb8dbe6c593435645edb728b4
zhalgas-seidazym <zhalgas.seidazym2005@gmail.com>
2025-05-11 22:43:08 +0500

    fully corrected drf

--- eventify/models.py
+++ eventify/models.py
@@ -1,13 +1,23 @@
 from django.core.files.storage import default_storage
 from django.db import models
 from django.contrib.auth.models import User
+from enum import Enum
+
+class EventStatusEnum(Enum):
+    PENDING = 'pending'
+    APPROVED = 'approved'
+    REJECTED = 'rejected'
+
+    @classmethod
+    def choices(cls):
+        return [(member.value, member.name.title().replace("_", " ")) for member in cls]
 
 class Event(models.Model):
-    STATUS_CHOICES = [
-        ('pending', 'Pending Approval'),
-        ('approved', 'Approved'),
-        ('rejected', 'Rejected'),
-    ]
+    # STATUS_CHOICES = [
+    #     ('pending', 'Pending Approval'),
+    #     ('approved', 'Approved'),
+    #     ('rejected', 'Rejected'),
+    # ]
 
     title = models.CharField(max_length=255)
     description = models.TextField()
@@ -16,8 +26,16 @@ class Event(models.Model):
     organizer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="organized_events")
     categories = models.ManyToManyField('Category', related_name="events")
     cover = models.ImageField(null=True, blank=True, storage=default_storage)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')  # Default: needs approval
+    status = models.CharField(
+        max_length=10,
+        choices=EventStatusEnum.choices(),
+        default=EventStatusEnum.PENDING.value
+    )
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def status_display(self):
+        return self.get_status_display()
 
     def __str__(self):
         return f"{self.title} ({self.get_status_display()})"
